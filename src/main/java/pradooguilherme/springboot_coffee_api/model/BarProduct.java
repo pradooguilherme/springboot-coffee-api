e62ce614d06a41dc2b866a80916bad2ae5261544
pradooguilherme <guipradu2020@gmail.com>
2026-01-25 20:24:54 -0300

    feat(product): add BarProduct entity

---
+++ src/main/java/pradooguilherme/springboot_coffee_api/model/BarProduct.java
@@ -0,0 +1,32 @@
+package pradooguilherme.springboot_coffee_api.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "bar_products")
+public class BarProduct extends Product {
+
+    @Column(name = "bar_product_final_price")
+    private Double finalPrice;
+
+    @Column(name = "bar_product_size")
+    private BarProductSize barProductSize;
+
+    public Double getFinalPrice() {
+        return finalPrice;
+    }
+
+    public void setFinalPrice(Double finalPrice) {
+        this.finalPrice = finalPrice;
+    }
+
+    public BarProductSize getBarProductSize() {
+        return barProductSize;
+    }
+
+    public void setBarProductSize(BarProductSize barProductSize) {
+        this.barProductSize = barProductSize;
+    }
+}
