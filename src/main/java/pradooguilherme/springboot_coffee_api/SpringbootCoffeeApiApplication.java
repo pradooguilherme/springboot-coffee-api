b4001d6734bf24de9b79aa9a7206d2c51d5c2c72
pradooguilherme <guipradu2020@gmail.com>
2026-01-25 01:22:00 -0300

    First commit of Spring boot Coffe API

---
+++ src/main/java/pradooguilherme/springboot_coffee_api/SpringbootCoffeeApiApplication.java
@@ -0,0 +1,13 @@
+package pradooguilherme.springboot_coffee_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCoffeeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCoffeeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pradooguilherme/springboot_coffee_api/SpringbootCoffeeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pradooguilherme.springboot_coffee_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCoffeeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
