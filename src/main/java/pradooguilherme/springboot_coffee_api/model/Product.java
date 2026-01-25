b4db011d84ce3f305b27007203f481a7018c92cf
pradooguilherme <guipradu2020@gmail.com>
2026-01-25 20:24:32 -0300

    feat(product): add Product entity

---
+++ src/main/java/pradooguilherme/springboot_coffee_api/model/Product.java
@@ -0,0 +1,54 @@
+package pradooguilherme.springboot_coffee_api.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_code")
+    private String code;
+
+    @Column(name = "product_name")
+    private String name;
+
+    @Column(name = "product_description")
+    private String description;
+
+    @Column(name = "product_price")
+    private Double price;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
