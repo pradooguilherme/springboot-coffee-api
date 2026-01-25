cc7dde217cc8113a6f9f64679701990419ecc467
pradooguilherme <guipradu2020@gmail.com>
2026-01-25 19:16:54 -0300

    chore: adds SpringbootCoffeeApiApplication.java to the project

--- src/main/java/pradooguilherme/springboot_coffee_api/SpringbootCoffeeApiApplication.java
+++ src/main/java/pradooguilherme/springboot_coffee_api/SpringbootCoffeeApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootCoffeeApiApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootCoffeeApiApplication.class, args);
 	}
-
 }
